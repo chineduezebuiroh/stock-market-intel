c8c8ebfd63bbfc2ed228cc0541d9811225198221
chineduezebuiroh <c.eze29@gmail.com>
2025-12-13 20:35:20 -0500

    Update run_stocks_weekly_rollup_guarded.py

--- .github/scripts/run_stocks_weekly_rollup_guarded.py
+++ .github/scripts/run_stocks_weekly_rollup_guarded.py
@@ -100,8 +100,8 @@ def run_profile() -> None:
     """
 
     results = []
-    results += run_combo_health(combos=["stocks_c_dwm_shortlist"], universe="shortlist_stocks.csv")
-    results += run_combo_health(combos=["stocks_c_dwm_all"], universe="options_eligible.csv")
+    results += run_combo_health(combos=["stocks_b_wmq_shortlist"], universe="shortlist_stocks.csv")
+    results += run_combo_health(combos=["stocks_b_wmq_all"], universe="options_eligible.csv")
     print_results(results)
 
 
